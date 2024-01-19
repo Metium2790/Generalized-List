433eb37c1c2273f5a39f1c908a1779c0efb6093b
Metium <113807366+Metium2790@users.noreply.github.com>
2024-01-19 18:10:41 +0330

    Fixed/Added the function to add new down links by user

---
+++ Generalized_List v1.1/Generalized_List/LinkedList.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generalized_List
+{
+    public class Node //node class
+    {
+        public string data;
+        public bool flag;
+        public Node link;
+        public Node blink; //usage of the double way loop linked list
+        public GList dlink;
+
+        public Node(string d)
+        {
+            data = d;
+            flag = false;
+            link = null;
+            blink = null;
+            dlink = null;
+        }
+
+    }
+
+    public class looplist //double way loop linked list
+    {
+        private Node first;
+
+        public void Add(Node d = null, looplist ll = null)
+        {
+            if (d != null)
+            {
+                if (first == null)
+                {
+                    first = d;
+                    while(d.link!=null)
+                    {
+                        d = d.link;
+                    }
+                    d.link = first;
+                    first.blink = d;
+                }
+                else
+                {
+                    Node p = first;
+                    while (p.link != first)
+                    {
+                        p = p.link;
+                    }
+                    p.link = d;
+                    d.blink = p;
+                    d.link = first;
+                }
+            }
+            else if (ll != null)
+            {
+                if (first == null)
+                {
+                    first = ll.first;
+                }
+                else
+                {
+                    Node q = ll.first;
+                    Node p = first;
+                    while (p.link != first)
+                    {
+                        p = p.link;
+                    }
+                    while (q.link != first)
+                    {
+                        q = q.link;
+                    }
+                    q.link = first;
+                    first.blink = q;
+
+                    p.link = ll.first;
+                    ll.first.blink = p;
+                }
+            }
+        }
+        public void Display() //prints out the list
+        {
+            Node p = first;
+            Console.Write("<"+p.data+",");
+            p = p.link;
+            while (p.link != first)
+            {
+                Console.Write(p.data+",");
+                p = p.link;
+            }
+            Console.Write(p.data+">");
+        }
+    }
+
+    public class GList //generalized list
+    {
+        private Node first;
+
+        public void Add(Node d=null,GList l=null) //adds to the list
+        {   if(d!=null)
+            {
+                if (first != null)
+                {
+                    Node p = first;
+                    while (p.link != null)
+                    {
+                        p = p.link;
+                    }
+                    p.link = d;
+                }
+                else
+                {
+                    first = d;
+                }
+            }
+            else if (l != null)
+            {
+                if (first != null)
+                {
+                    Node p = first;
+                    while (p.link != null)
+                    {
+                        p = p.link;
+                    }
+                    p.link = l.first;
+                }
+                else
+                {
+                    first = l.first;
+                }
+            }
+        }
+
+
+        public void Add_dlink(Node p, Node d=null,GList l=null) //adds to the down list of the chosen node
+        {
+            if (d != null)
+            {
+                if (p.flag == false)
+                {
+                    p.flag = true;
+                    GList newlink = new GList();
+
+                    newlink.Add(d);
+
+                    p.dlink = newlink;
+                }
+                else
+                {
+                    p.dlink.Add(d);
+                }
+            }
+            else if (l != null)
+            {
+                if (p.flag == false)
+                {
+                    p.flag = true;
+
+                    p.dlink = l;
+                }
+                else
+                {
+                    p.dlink.Add(l: l);
+                }
+            }
+        }
+
+        public void Display() //prints out the list
+        {
+            Node p = first;
+            Console.Write("<");
+            if (p != null)
+            {
+                while (p != null)
+                {
+                    Console.Write(p.data);
+                    if (p.flag)
+                    {
+                        p.dlink.Display();
+                    }
+                    if(p.link != null)
+                        Console.Write(",");
+                    p = p.link;
+                }
+                Console.Write(">");
+            }
+        }
+
+        public int Depth() //finds the depth of the list and returns it
+        {
+            int dep = 0;
+            int n = 0;
+            Node p = first;
+
+            while(p!=null)
+            {
+                if (p.flag == true)
+                {
+                    n = p.dlink.Depth();
+                    if (n > dep)
+                    {
+                        dep = n;
+                    }
+                }
+                p = p.link;
+            }
+            return dep + 1;
+        }
+
+        public int Allnodes() //finds the total amount of the nodes and returns it
+        {
+            int n = 0;
+            Node p = first;
+
+            while (p != null)
+            {
+                if (p.flag == true)
+                {
+                    n += p.dlink.Allnodes();
+                }
+                p = p.link;
+            }
+            return n;
+        }
+
+        public void delete(string x) //delete out a data from the list
+        {
+            Node q = null;
+            Node p = first;
+            while(p!=null)
+            {
+                if (p.flag == true)
+                {
+                    p.dlink.delete(x);
+                }
+                if (p.data == x)
+                {
+                    if (q != null)
+                    {
+                        q.link = p.link;
+                        p.link = null;
+                        p = q.link;
+                    }
+                    else
+                    {
+                        first = p.link;
+                        p.link = null;
+                        p = first;
+                    }
+                }
+                else
+                {
+                    q = p;
+                    p = p.link;
+                }
+            }
+        }
+
+        public looplist turntoloop() //turns a generalized list to a looplist
+        {
+            looplist list = new looplist();
+            Node g = first;
+            
+            while (g != null && g.link!=first)
+            {
+                if (g.flag == true)
+                {
+                    list.Add(ll:g.dlink.turntoloop());
+                }
+                Node p = new Node("");
+                p = g;
+                list.Add(p);
+                g = g.link;
+            }
+            return list;
+        }
+
+
+        public bool Comparison(GList L) //compares 2 lists with each other
+        {
+            bool result = false;
+            Node p = first;
+            Node q = L.first;
+
+            while (q != null && p != null)
+            {
+                if (q.flag == true && p.flag == true)
+                {
+                    if (p.dlink.Comparison(q.dlink) == false)
+                    {
+                        result = false;
+                        break;
+                    }
+                    else
+                    {
+                        result = p.dlink.Comparison(q.dlink);
+                        if (p.data == q.data)
+                            result = true;
+                        q = q.link;
+                        p = p.link;
+                    }
+                }
+                else if (q.flag == false && p.flag == false)
+                {
+                    if (q.data != p.data)
+                    {
+                        result = false;
+                        break;
+                    }
+                    else
+                    {
+                        result = true;
+                    }
+                    q = q.link;
+                    p = p.link;
+
+                }
+                else
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public Node FindNode(int n) //A function to find a node inside the list
+        {
+            Node p = first;
+            int temp = n;
+
+            if (n == -1)
+            {
+                Console.WriteLine("Proccess has been canceled");
+                return null;
+            }
+
+            if (p != null)
+            {
+                while (temp > 0)
+                {
+                    p = p.link;
+                    temp--;
+                }
+                if (p != null)
+                {
+                    if (p.flag == true)
+                    {
+                        Console.WriteLine("The node you chose already has down link. do you want to add down link to one of its nodes ?(No=-1)(Yes=enter the number)");
+                        int number = Int32.Parse(Console.ReadLine());
+                        if (number == -1)
+                        {
+                            return p;
+                        }
+                        return p.dlink.FindNode(number);
+                    }
+                    else
+                    {
+                        return p;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You reached out of the list");
+                    return null;
+                }
+            }
+            else
+            {
+                Console.WriteLine("The List is empty");
+                return null;
+            }
+        }
+
+    }
+
+}
---
+++ Generalized_List v1.1/Generalized_List/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generalized_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //simple command access
+            Console.WriteLine("1-Make a list(20 max)");
+            Console.WriteLine("2-Add to List");
+            Console.WriteLine("3-Add to Down List");
+            Console.WriteLine("4-Find depth of the list");
+            Console.WriteLine("5-Print the list");
+            Console.WriteLine("6-Find all amounts of the nodes");
+            Console.WriteLine("7-Find a data and delete them from the list");
+            Console.WriteLine("8-Make a double list and add all the list item to it");
+            Console.WriteLine("9-Compare 2 list to each other");
+            Console.WriteLine("0-EXIT");
+
+            GList[] lists = new GList[20]; //array to save lists
+            looplist[] looplists = new looplist[20];
+            int listnum = -1;
+            int listnum2 = -1;
+            int list = 0; //amount of active lists
+            int looplist = 0;
+
+            while (true)
+            {
+                Console.WriteLine("----------------------------------------------------------------");
+                Console.Write("LISTS: ");
+                for (int i = 0; i < list; i++)
+                {
+                    Console.Write((i+1)+",");
+                }
+                Console.WriteLine("");
+                Console.Write("LOOP LISTS: ");
+                for (int i = 0; i < looplist; i++)
+                {
+                    Console.Write((i + 1) + ",");
+                }
+
+                Console.WriteLine("");
+                string CMD = Console.ReadLine();
+
+                switch (CMD)
+                {
+                    case "1":
+                        lists[list] = new GList();
+                        list++;
+                        Console.WriteLine("List maded!!");
+                        break;
+                    case "3":
+                        Console.WriteLine("which list you want to add choose?");
+                        listnum = Int32.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Please give us the number of your node inside the list which you want to add a down list to it(first=0)(cancel=-1)");
+                        int number = Int32.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Your Data inside the new node: ");
+                        Node newnode = new Node(Console.ReadLine());
+
+                        lists[listnum - 1].Add_dlink(lists[listnum - 1].FindNode(number), newnode);
+                        break;
+                    case "2":
+                        Console.WriteLine("which list you want to add choose?");
+                        listnum = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Your Data inside the new node: ");
+                        Node newnode2 = new Node(Console.ReadLine());
+                        lists[listnum-1].Add(newnode2);
+                        Console.WriteLine("ADDED");
+                        break;
+                    case "4":
+                        Console.WriteLine("which list you want to find choose?");
+                        listnum = Int32.Parse(Console.ReadLine());
+                        lists[listnum-1].Depth();
+                        break;
+                    case "5":
+                        Console.WriteLine("which list you want to choose?");
+                        listnum = Int32.Parse(Console.ReadLine());
+                        lists[listnum-1].Display();
+                        Console.WriteLine("");
+                        break;
+                    case "6":
+                        Console.WriteLine("which list you want to choose?");
+                        listnum = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Amount of nodes: "+lists[listnum-1].Allnodes());
+                        break;
+                    case "7":
+                        Console.WriteLine("which list you want to choose?");
+                        listnum = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("What's the data you want to delete?");
+                        string deldata = Console.ReadLine();
+                        lists[listnum-1].delete(deldata);
+                        break;
+                    case "8":
+                        Console.WriteLine("which list you want to choose?");
+                        listnum = Int32.Parse(Console.ReadLine());
+                        looplists[looplist] = lists[listnum - 1].turntoloop();
+                        lists[listnum - 1] = lists[listnum];
+                        looplist++;
+                        list--;
+                        looplists[looplist-1].Display();
+                        Console.WriteLine("");
+                        break;
+                    case "9":
+                        Console.WriteLine("which 1st list you want to choose?");
+                        listnum = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("which 2nd list you want to choose?");
+                        listnum2 = Int32.Parse(Console.ReadLine());
+                        Console.Write("Are they equal ? " + lists[listnum-1].Comparison(lists[listnum2-1]));
+                        break;
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please use the right command");
+                        break;
+                }
+            }
+
+
+
+        }
+    }
+}//Coded by Mohammad Mahdi Mohammadi (AKA Metium)
